f6d8b5d339bd451de03a288f5eff98243d553615
Nicholas Young <ndy14@ufl.edu>
2019-01-22 23:06:14 -0500

    Now communicates with parser on local network. Make sure you have configured the docker network 'jist-net '

--- run.sh
+++ run.sh
@@ -1,2 +1,1 @@
-docker run -p 5000:80 -it --rm --name jist-summarizer-container jist-summarizer-image:v0.0.1
-
+docker run -p 5000:80 --rm --network="jist-net" --name jist-summarizer-container jist-summarizer-image:v0.0.1

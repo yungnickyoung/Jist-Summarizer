fedee2037071c37ad01a48cfb85c54fdbfbe80a0
Nicholas Young <ndy14@ufl.edu>
2019-01-24 23:58:56 -0500

    Now properly accepts POST data and returns the summary. Communication between the 3 modules is working

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from summa.summarizer import summarize
-from flask import Flask
-from flask import request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/summarize')
+@app.route('/summarize', methods = ['POST'])
 def summarizeArticle():
-	# print(request.data.decode("unicode-escape"))
-	summary = summarize(request.data.decode("unicode-escape"), ratio=.1)
-	app.logger.info("Processing article %s", summary)
-	return summary
+	req_data = request.get_json()
+
+	app.logger.info('Domain: ' + req_data['domain'])
+	summary = summarize(req_data['full_text'], ratio = 0.1)
+
+	app.logger.info(summary)
+
+	response = { 'summary': summary }
+	return jsonify(response), 200
 
 if __name__ == "__main__":
 	app.run(debug=True, host='0.0.0.0', port=80)

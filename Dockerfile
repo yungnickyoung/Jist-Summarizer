7e384ccc7bcd34f3a99786520c4853e511af2992
Nicholas Young <ndy14@ufl.edu>
2019-03-06 22:41:09 -0500

    Fix port on Dockerfile. Add endpoint documentation. Documentation is now more RESTful (aborts with different HTTP codes). Refactored to communicate independently from the HTML parser

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,6 @@ WORKDIR /usr/src/app
 COPY . /usr/src/app
 RUN pip install --no-cache-dir -r requirements.txt
 
-EXPOSE 80
+EXPOSE 5002
 
 CMD [ "python", "main.py" ]
--- main.py
+++ main.py
@@ -4,17 +4,31 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+## /summarize -- POST
+# Required body data: 
+#   - article_text
+# 
+# Returns:
+#   - 200: If article text was successfully summarized
+#   - 400: If request data is missing or invalid
+#   - 500: If problem occurred during summarization
 @app.route('/summarize', methods = ['POST'])
 def summarizeArticle():
-	req_data = request.get_json()
+    requestData = request.get_json()
 
-	app.logger.info('Domain: ' + req_data['domain'])
-	summary = summarize(req_data['full_text'], ratio = 0.1)
+    # Validate required data
+    try:
+        articleText = requestData['article_text']
+    except KeyError as kerr:
+        abort(Response(status=400, response="400: Error(400): Missing or invalid data provided: {}".format(str(kerr))))
 
-	app.logger.info(summary)
+    try:
+        summary = summarize(articleText, ratio = 0.1)
+    except Exception as e:
+        abort(Response(status=500, response="500: Error (500): " + str(e)))
 
-	response = { 'summary': summary }
-	return jsonify(response), 200
+    data = { 'summary': summary }
+    return jsonify(data), 200
 
 if __name__ == "__main__":
-	app.run(debug=True, host='0.0.0.0', port=5002)
+    app.run(debug=True, host='0.0.0.0', port=5002)
